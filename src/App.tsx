02f8343b2ba8b70f7b5f7a271e760eda344a4e82
Mars375 <loic_2610@hotmail.fr>
2024-07-22 16:27:37 +0200

    feat: test dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
 import * as React from "react";
-import { DatePicker } from "@/main";
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuLabel,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuCheckboxItem,
+} from "@/components/ui/DropdownMenu/DropdownMenu"; // Assurez-vous que le chemin d'importation est correct
 
 const App: React.FC = () => {
+	const [notificationSetting, setNotificationSetting] =
+		React.useState("enabled");
+	const [darkMode, setDarkMode] = React.useState(true);
+
 	return (
-		<div className='flex h-screen items-center justify-around flex-col'>
-			<DatePicker showOutsideDays={true} locale='fr-FR' />
+		<div className='flex h-screen items-center justify-center'>
+			<DropdownMenu>
+				<DropdownMenuTrigger className='px-4 py-2 text-white bg-blue-500 rounded-md'>
+					Options
+				</DropdownMenuTrigger>
+				<DropdownMenuContent sideOffset={5}>
+					<DropdownMenuLabel className='text-gray-900'>
+						Paramètres
+					</DropdownMenuLabel>
+					<DropdownMenuItem onSelect={() => alert("Profil sélectionné")}>
+						Profil
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuLabel className='text-gray-900'>
+						Notifications
+					</DropdownMenuLabel>
+					<DropdownMenuRadioGroup
+						value={notificationSetting}
+						onValueChange={setNotificationSetting}
+					>
+						<DropdownMenuRadioItem value='enabled'>
+							Activées
+						</DropdownMenuRadioItem>
+						<DropdownMenuRadioItem value='disabled'>
+							Désactivées
+						</DropdownMenuRadioItem>
+					</DropdownMenuRadioGroup>
+					<DropdownMenuSeparator />
+					<DropdownMenuLabel className='text-gray-900'>
+						Apparence
+					</DropdownMenuLabel>
+					<DropdownMenuCheckboxItem
+						checked={darkMode}
+						onCheckedChange={(checked) => setDarkMode(checked === true)}
+					>
+						Mode Sombre
+					</DropdownMenuCheckboxItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem onSelect={() => alert("Déconnexion sélectionnée")}>
+						Déconnexion
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
 		</div>
 	);
 };
